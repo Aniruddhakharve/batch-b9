ce3b23fff8ed200ba4b34f2f6d12590947e4c993
Aniruddha Kharve <kharveayash@gmail.com>
2025-05-14 11:45:19 +0530

    ohh

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0e35ddab05955cf57"
   instance_type = var.instance_type
 key_name = "mumbai-01"
-vpc_security_group_ids = data.aws_security_group.mysg.id
+vpc_security_group_ids = [data.aws_security_group.mysg.id] 
   tags = {
     Name =  var.project_name
 
